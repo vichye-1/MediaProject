016e82384ecefcab5780fc9087525be78687e479
knockknock <yangban11111@gmail.com>
2024-06-04 16:59:06 +0900

    import SnapKit, label 자리 잡기, code base 기본 틀

--- MediaProject/ViewController.swift
+++ MediaProject/ViewController.swift
@@ -6,12 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let userNameLabel = UILabel()
+    let movieMainImageview = UIImageView()
+    let playButton = UIButton()
+    let favoriteListButton = UIButton()
+    let hotMovieLabel = UILabel()
+    let hotMovie1 = UIImageView()
+    let hotMovie2 = UIImageView()
+    let hotmoview3 = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(userNameLabel)
+        view.addSubview(movieMainImageview)
+        view.addSubview(playButton)
+        view.addSubview(favoriteListButton)
+        view.addSubview(hotMovieLabel)
+        view.addSubview(hotMovie1)
+        view.addSubview(hotMovie2)
+        view.addSubview(hotmoview3)
+    }
+    
+    func configureLayout() {
+        userNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.equalTo(16)
+            make.trailing.equalTo(-16)
+            make.height.equalTo(24)
+        }
+        
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .gray
+        userNameLabel.backgroundColor = .white
+        movieMainImageview.backgroundColor = .white
     }
 
 
