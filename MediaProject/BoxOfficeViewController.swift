f5c5142ed161e323814454bdbb89fd5f51f1a7b0
knockknock <yangban11111@gmail.com>
2024-06-08 21:47:33 +0900

    textField, searchButton 위치 설정

--- MediaProject/BoxOfficeViewController.swift
+++ MediaProject/BoxOfficeViewController.swift
@@ -12,6 +12,7 @@ import SnapKit
 class BoxOfficeViewController: UIViewController {
     
     var movieTextField = UITextField()
+    let searchButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +22,30 @@ class BoxOfficeViewController: UIViewController {
     }
     
     func configureHierarchy() {
-        
+        view.addSubview(movieTextField)
+        view.addSubview(searchButton)
     }
     
     func configureLayout() {
+        movieTextField.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.height.equalTo(48)
+        }
         
+        searchButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.equalTo(movieTextField.snp.trailing).offset(8)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.width.equalTo(70)
+            make.height.equalTo(48)
+        }
     }
     
     func configureUI() {
-        view.backgroundColor = .darkGray
+        view.backgroundColor = .white
+        movieTextField.backgroundColor = .darkGray
+        searchButton.backgroundColor = .brown
     }
     
 
